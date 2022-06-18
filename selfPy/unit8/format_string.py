3333026601425f31a9592a77ea349c523114e390
Moshe <moshe.dvora@gmail.com>
2022-06-18 22:42:50 +0300

    Solved Formating

--- selfPy/unit8/format_string.py
+++ selfPy/unit8/format_string.py
@@ -1,4 +1,4 @@
 data = ("self", "py", 1.543)
-format_string = "Hello"
+format_string = "Hello %s.%s learner, you have only %.1f units left before you master the course!"
 
-print(format_string % data)
+print(format_string %data)
