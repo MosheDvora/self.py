6b70bdc57de178b486c545712e518cea21476259
Moshe <moshe.dvora@gmail.com>
2022-07-02 17:18:34 +0300

    solve Mult Tuple

--- selfPy/unit8/multi tuple.py
+++ selfPy/unit8/multi tuple.py
@@ -3,6 +3,12 @@ second_tuple = (4, 5)
 
 
 def mult_tuple(tuple1, tuple2):
+    list_of_tuple = []
+    for t1 in tuple1:
+        for t2 in tuple2:
+            list_of_tuple.append((t1, t2))
+    print(tuple(list_of_tuple))
+
 
 
 
